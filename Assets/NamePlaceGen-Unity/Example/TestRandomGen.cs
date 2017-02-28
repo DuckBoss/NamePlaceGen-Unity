9cb027fe6aba1be4d6f2f5957ed27bc753a85656
JasonJerome <jasonjerome16@gmail.com>
2017-02-28 17:37:40 -0500

    v1.0.0
    Project Upload

---
+++ Assets/NamePlaceGen-Unity/Example/TestRandomGen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRandomGen : MonoBehaviour {
+
+	RandomNameGen genScript;
+
+	// Use this for initialization
+	void Start () {
+		genScript = GetComponent<RandomNameGen>();
+
+		Debug.Log(string.Format("Random Male Name Gen: \n{0} {1} {2}", genScript.GenerateFirstNameMale(), genScript.GenerateMiddleName(), genScript.GenerateLastName()));
+		Debug.Log(string.Format("Random Female Name Gen: \n{0} {1} {2}", genScript.GenerateFirstNameFemale(), genScript.GenerateMiddleName(), genScript.GenerateLastName()));
+		Debug.Log(string.Format("Random Name Gen: \n{0} {1} {2}", genScript.GenerateFirstName(), genScript.GenerateMiddleName(), genScript.GenerateLastName()));
+	}
+
+}
---
+++ Assets/NamePlaceGen-Unity/Scripts/RandomNameGen.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class RandomNameGen : MonoBehaviour {
+	
+	private enum Gender {
+		Male,
+		Female,
+		None
+	}
+	[SerializeField]
+	private Gender genderType;
+	private List<String> fileContent = new List<String>();
+
+	public string textFile;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public string GenerateFirstNameMale() {
+		fileContent.Clear();
+		LoadFile("NAMES-M.txt");
+
+		UnityEngine.Random.InitState((int)System.DateTime.Now.Ticks);
+		int genInt = UnityEngine.Random.Range(0, fileContent.Count);
+		return fileContent[genInt]; 
+	}
+
+	public string GenerateFirstNameFemale() {
+		fileContent.Clear();
+		LoadFile("NAMES-F.txt");
+
+		UnityEngine.Random.InitState((int)System.DateTime.Now.Ticks);
+		int genInt = UnityEngine.Random.Range(0, fileContent.Count);
+		return fileContent[genInt]; 
+	}
+
+	public string GenerateFirstName() {
+		fileContent.Clear();
+		LoadFile("NAMES-F.txt");
+
+		UnityEngine.Random.InitState((int)System.DateTime.Now.Ticks);
+		int genInt = UnityEngine.Random.Range(0, fileContent.Count);
+		return fileContent[genInt]; 
+	}
+
+	public string GenerateLastName() {
+		fileContent.Clear();
+		LoadFile("NAMES.txt");
+
+		UnityEngine.Random.InitState((int)System.DateTime.Now.Ticks);
+		int genInt = UnityEngine.Random.Range(0, fileContent.Count);
+		return fileContent[genInt]; 
+	}
+
+	public string GenerateMiddleName() {
+		fileContent.Clear();
+		LoadFile("NAMES.txt");
+
+		UnityEngine.Random.InitState((int)System.DateTime.Now.Ticks);
+		int genInt = UnityEngine.Random.Range(0, fileContent.Count);
+		return fileContent[genInt]; 
+	}
+
+	public string GeneratePlace() {
+		fileContent.Clear();
+		LoadFile("PLACES.txt");
+
+		UnityEngine.Random.InitState((int)System.DateTime.Now.Ticks);
+		int genInt = UnityEngine.Random.Range(0, fileContent.Count);
+		return fileContent[genInt]; 
+	}
+
+	public void LoadFile(string fileName) {
+		try {
+			string curLine;
+			StreamReader newReader = new StreamReader("Assets/NamePlaceGen-Unity/Resources/" + fileName);
+
+			using (newReader) {
+				do {
+					curLine = newReader.ReadLine();
+					fileContent.Add(curLine);
+				}
+				while(curLine != null);
+				newReader.Close();
+			}
+		}
+		catch(Exception e) {
+			Debug.Log(string.Format("{0}\n", e.ToString()));
+		}
+	}
+
+	public void DisplayFileContents() {
+		foreach(string item in fileContent) {
+			Debug.Log(item);
+		}
+	}
+
+}
